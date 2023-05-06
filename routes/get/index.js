f03afa17e207fe1bd8434ced5664b557cf9d6bd1
lizhengxin <1558268927@qq.com>
2023-05-06 18:34:25 +0800

    5-6 修改

--- routes/get/index.js
+++ routes/get/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/test', function(req, res, next) {
-  res.send('get/test 请求成功');
+  res.send({
+    code: 200,
+    msg: 'get/test 请求成功',
+  });
 });
 
 module.exports = router;
--- routes/post/index.js
+++ routes/post/index.js
@@ -1,12 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
+// 上传文件模块
+const multiparty = require('multiparty')
+// 文件操作模块
+const fs = require('fs')
+
+
+
 /* GET users listing. */
-router.post('/test', function(req, res, next) {
-  res.send('/post/test 请求成功');
+router.post('/test', function (req, res, next) {
+    res.send({
+      code: 200,
+      msg: '/post/test 请求成功'
+    });
 });
 
-router.post('/ttt', function(req, res, next) {
-  res.send('/post/ttt 请求成功');
+
+
+// 分片上传文件
+router.post('/uploadFile', function (req, res, next) {
+    let form = new multiparty.Form();
+    //这里可以设置图片上传的路径，默认为当前用户下的temp文件夹
+    form.uploadDir = "/static/img";
+    form.parse(req, function(err, fields, files) {
+        //files即为上传图片的信息
+        console.log(fields, files)
+        if(!err){
+            res.send({
+                code: 200,
+                msg: '成功',
+                err
+            });
+        } else {
+            res.send({
+                code: 400,
+                msg: '失败',
+                err
+            });
+        }
+    });
+
 });
+
+
+
+
+
 module.exports = router;
