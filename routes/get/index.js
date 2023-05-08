8c37054e08e042b3ea4f7bf72996469401f47a3d
lizhengxin <1558268927@qq.com>
2023-05-08 18:52:27 +0800

    大文件分片上传功能

--- routes/get/index.js
+++ routes/get/index.js
@@ -1,12 +1,53 @@
 var express = require('express');
 var router = express.Router();
+const { Buffer } = require('buffer')
+const fs = require('fs')
+const path = require('path')
 
-/* GET users listing. */
-router.get('/test', function(req, res, next) {
-  res.send({
-    code: 200,
-    msg: 'get/test 请求成功',
-  });
-});
+// 上传文件的临时路径
+const STATIC_TEMPORARY = path.join(__dirname, '../../static/temporary')
+// 文件路径
+const STATIC_FILES = path.join(__dirname, '../../static/files')
+
+
+// 合并切片接口
+router.get('/mergeFile', async (req, res) => {
+    const { filename } = req.query
+    try {
+        let len = 0
+        const bufferList = fs.readdirSync(`${STATIC_TEMPORARY}/${filename}`).map((hash,index) => {
+            const buffer = fs.readFileSync(`${STATIC_TEMPORARY}/${filename}/${hash}`)
+            len += buffer.length
+            return buffer
+        });
+        // 合并文件
+        const buffer = Buffer.concat(bufferList, len);
+        const ws = fs.createWriteStream(`${STATIC_FILES}/${filename}`)
+        ws.write(buffer);
+        ws.close();
+        // 删除之前的文件切片
+        res.send({
+            code: 200,
+            msg: '合并成功',
+            data: {
+                filename,
+            },
+            err: null
+        });
+    } catch (err) {
+        res.send({
+            code: 500,
+            msg: '合并失败',
+            data: {
+                filename,
+            },
+            err
+        });
+    }
+})
+
+
+
+
 
 module.exports = router;
--- routes/post/index.js
+++ routes/post/index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+const { Buffer } = require('buffer')
 // 上传文件模块
 const multiparty = require('multiparty')
 // 文件操作模块
 const fs = require('fs')
+const path = require('path')
+// 文件路径
+const STATIC_FILES = path.join(__dirname, '../../static/files')
+// 上传文件的临时路径
+const STATIC_TEMPORARY = path.join(__dirname, '../../static/temporary')
 
 
-
-/* GET users listing. */
 router.post('/test', function (req, res, next) {
     res.send({
       code: 200,
@@ -20,31 +23,42 @@ router.post('/test', function (req, res, next) {
 
 // 分片上传文件
 router.post('/uploadFile', function (req, res, next) {
-    let form = new multiparty.Form({uploadDir: './files'});
+    let form = new multiparty.Form();
     //这里可以设置图片上传的路径，默认为当前用户下的temp文件夹
-    // form.uploadDir = "./files";
     form.parse(req, function(err, fields, files) {
-        //files即为上传图片的信息
-        console.log(fields, files)
-        if(!err){
-            console.log('1===', fields);
-            console.log('2===', files);
-            let upFile = files.file[0]
-            // 重命名
-            res.send({
+        try {
+            let filename = fields.filename[0]; // 文件名
+            let hash = fields.hash[0]; // hash 标识
+            let chunk = files.chunk[0]; // 数据
+            let dir = `${STATIC_TEMPORARY}/${filename}`;
+            if (!fs.existsSync(dir)) { // fs.existsSync 以同步的方法检测目录是否存在，如果目录存在 返回 true ，如果目录不存在 返回false
+                fs.mkdirSync(dir); //  fs.mkdirSync() 方法用于同步创建目录。
+            }
+            const buffer = fs.readFileSync(chunk.path); // 同步读取文件
+            const ws = fs.createWriteStream(`${dir}/${hash}`); // 用于快速创建可写流，以将数据写入文件
+            ws.write(buffer); // 写入数据
+            ws.close(); // 关闭读写流
+            res.status(200).send({
                 code: 200,
                 msg: '上传成功',
-                fileSize: ((upFile.size) / 1048576).toFixed(2) + 'M'
+                data: {
+                    filename,
+                    hash
+                },
+                err: null
             })
-        } else {
-            res.send({
-                code: 400,
-                msg: '失败',
+        } catch (err) {
+            res.status(500).send({
+                code: 500,
+                msg: '上传失败',
+                data: {
+                    filename,
+                    hash
+                },
                 err
-            });
+            })
         }
-    });
-
+    })
 });
 
 
